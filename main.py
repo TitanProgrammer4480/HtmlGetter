adf9cc64742230b0baa042155ba06ef11248a513
ZonaryTitan <106260496+TitanProgrammer4480@users.noreply.github.com>
2024-11-27 10:44:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import requests
 
+header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"}
+
 if 'html' not in st.session_state:
   st.session_state.html = ''
 
@@ -10,5 +12,5 @@ with st.form(key="req_html"):
   url = st.text_input(label="Url:")
   submit = st.form_submit_button(label="Submit")
   if submit:
-    st.session_state.html = requests.get(url).text
+    st.session_state.html = requests.get(url, header=header).text
 st.code(st.session_state.html)
